c7fe1bf27aa177ac4e644207ef49f0a8a6d56b2a
Simon <simonarnold@live.de>
2020-11-20 13:51:32 +0100

    Funktion erweitert

--- Uebung02/data/circuit_matrices.m
+++ Uebung02/data/circuit_matrices.m
@@ -20,16 +20,22 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
   % Durchlaufe alle Elementtypen (R,L,C,...)
   for i=1:size(cirstruct.LCR,2)
     typ=cirstruct.LCR(i).parnames{1,1};
-
+    sz=size(cirstruct.LCR(i).pvmatrix,1);
+    
     % Durchlaufe alle Bauteile eines Typen
     for j=1:size(cirstruct.LCR(i).pvmatrix,1)
       val=cirstruct.LCR(i).pvmatrix(j,:);
       pins=cirstruct.LCR(i).vnmatrix(j,:);
       
+      
       % Implementierung fehlt
       if typ == 'R'
+        
         %Matrix pro gefundenem R um 1 Spalte vergrößern
         AR=[AR,sparse(n,1)];
+        %Diagonalmatrix Elemente einfügen
+        R=[R,zeros(sz,1)];
+        R(j,j)=1/val;
         %Richtung des Stroms bestimmen
         direction = pins(1) - pins(2);
         
@@ -53,6 +59,9 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
       
       if typ == 'L'
         AL=[AL,sparse(n,1)]
+        %Diagonalmatrix Elemente einfügen
+        L=[L,zeros(sz,1)];
+        L(j,j)=val;
         %Richtung des Stroms bestimmen
         direction = pins(1) - pins(2);
         
@@ -76,6 +85,9 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
       
       if typ == 'C'
         AC=[AC,sparse(n,1)]
+        %Diagonalmatrix Elemente einfügen
+        C=[C,zeros(sz,1)];
+        C(j,j)=val;
         %Richtung des Stroms bestimmen
         direction = pins(1) - pins(2);
         
@@ -99,6 +111,7 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
       
       if typ == 'V'
         AV=[AV,sparse(n,1)]
+        
         %Richtung des Stroms bestimmen
         direction = pins(1) - pins(2);
         
@@ -109,6 +122,8 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
             AV(pins(1),j)=1;
           endif
           AV(pins(2),j)=-1;
+          %Spannungsquelle in Spaltenvektor hinzufügen
+          V=[V;-val];
           
         %Strom fließt von 2 nach 1
         else
@@ -117,6 +132,8 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
           if pins(2) != 0
             AV(pins(2),j)=-1;
           endif
+          %Spannungsquelle in Spaltenvektor hinzufügen
+          V=[V;val];
         endif
       endif
       
@@ -132,7 +149,7 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
             AI(pins(1),j)=1;
           endif
           AI(pins(2),j)=-1;
-          
+          I=[I;val];
         %Strom fließt von 2 nach 1
         else
           AI(pins(1),j)=1;
@@ -140,9 +157,11 @@ function [AC,AL,AR,AV,AI,C,L,R,V,I] = circuit_matrices(filename)
           if pins(2) != 0
             AI(pins(2),j)=-1;
           endif
+          I=[I;-val];
         endif
       endif
       
     end
   end
+
 end
