3fa4bfe5148aa9a0abeafaed8495ec14cc434746
Simon <simonarnold@live.de>
2020-12-10 22:13:49 +0100

    Aufgabe 5_3a+b

---
+++ Uebung05/data/Ag5_3b.m
@@ -0,0 +1,25 @@
+##R = 1:1:100;
+##C = zeros(1,100);
+##for i = 1 : 100
+##  C(1,i) = spherecapacity(R(1,i),1);
+##endfor
+plot(R,C,'linewidth',3);
+axis([1 100]);
+xlabel('Innenradius in cm','Fontsize',26);
+ylabel('Kapazität in F','Fontsize',26);
+set(gca,'Fontsize',26);
+
+##Yticks = get(gca,'Ytick');
+##YTickLabels = cellstr(num2str(round(log10(Yticks(:))), '10^%d'));
+##set(gca,'YTickLabel',YTickLabels);
+
+ytick=get(gca,'ytick');
+yticklab = [
+"0";
+strcat('0.5 \cdot ',num2str(9,'10^-^%d'));
+strcat('1 \cdot ',(num2str(9,'10^-^%d')));
+strcat('1.5 \cdot',(num2str(9,'10^-^%d')));
+strcat('2 \cdot',(num2str(9,'10^-^%d')))];
+yticklab = cellstr(yticklab);
+%yticklab = cellstr(num2str(round(-log10(ytick(:))), '10^-^%d'));
+set(gca,'YTick',ytick,'YTickLabel',yticklab,'TickLabelInterpreter','tex')
---
+++ Uebung05/data/spherecapacity.m
@@ -0,0 +1,51 @@
+
+function C = spherecapacity (R1, er)
+  R2 = R1 + 7;
+  openfemm;
+  newdocument(1);
+  ei_probdef('centimeters','axi',1.E-8,0,30);
+  ei_addmaterial('er',er,er,0);
+  %Conductor Props erstellen
+  ei_addconductorprop('Rand1',1,0,1);
+  ei_addconductorprop('Rand2',0,0,1);
+  
+  ei_drawarc(0,-R1,R1,0,90,90);
+  ei_drawarc(0,-R2,R2,0,90,90);
+  
+  ei_selectarcsegment(0,-R1);
+  ei_setarcsegmentprop(10,'<None>',0,0,'Rand1');
+  ei_copyrotate2(0,0,90,1,3);
+  ei_clearselected;
+  
+  ei_selectarcsegment(0,-R2);
+  ei_setarcsegmentprop(10,'<None>',0,0,'Rand2');
+  ei_copyrotate2(0,0,90,1,3);
+  ei_clearselected;
+  
+  ei_addblocklabel(R2-(R2-R1)/2,0);
+  ei_selectlabel(R2-(R2-R1)/2,0);
+  ei_setblockprop('er',1,0,0);
+  ei_clearselected;
+  
+  ei_addblocklabel(0,0);
+  ei_selectlabel(0,0);
+  ei_setblockprop('<No Mesh>',1,0,0);
+  ei_clearselected;
+  
+  ei_addsegment(0,R2,0,R1);
+  ei_addsegment(0,-R2,0,-R1);
+  
+  
+  %Speichere die Datei ab
+  ei_saveas('Aufgabe5_3.FEE');
+  
+  %Lade die gepeicherte Datei und erzeuge Simulation
+  ei_analyze(0);
+  ei_loadsolution;
+  
+  %Liegenden Vektor erstellen, Ladung / Spannung ergibt Kapazit\"at C
+  G = [0,0];
+  G = eo_getconductorproperties('Rand1');
+  C = G(1,2)/G(1,1);
+  
+endfunction
