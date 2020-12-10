decd90509229c554c754a2e90d2dee51cf6027cb
Dominik <dominik.schiller@stud.tu-darmstadt.de>
2020-12-10 12:04:35 +0100

    pushen für simon

--- Uebung05/data/Ag5_2b.m
+++ Uebung05/data/Ag5_2b.m
@@ -1,5 +1,14 @@
-C = zeros(20,1);
+%C = zeros(1,20);
 
-for i=1:20
-  C(i,1)= femmcapacity(30, 30, 1, 2, i);
-endfor
+%for i=1:20
+%  C(1,i)= femmcapacity(30, 30, 1, 2, i);
+%endfor
+
+N = 1:1:20;
+
+semilogy(N,C,'linewidth',3);
+
+xlabel('Anzahl Schichten','fontsize',26);
+ylabel('Kapazität in F','fontsize',26);
+set(gca,'Fontsize',26);
+axis([1 20]);
