e49cbac2b4ae76c4751ae83e5e230d0d8f56319e
Simon <simonarnold@live.de>
2021-01-05 10:40:24 +0100

    Auto stash before merge of "main" and "origin/main"
    Simon riesenpush

--- Uebung06/data/Aufgabe6_2a.m
+++ Uebung06/data/Aufgabe6_2a.m
@@ -1,4 +1,4 @@
-function C = Aufgabe6_2a (V)
+function F = Aufgabe6_2a (d, z, meshnet)
   openfemm;
   newdocument(1);
   ei_probdef('millimeters','axi',1.E-8,0,30);
@@ -9,8 +9,9 @@ function C = Aufgabe6_2a (V)
   ei_addmaterial('Porzellan', 6, 6, 0);
   
   ei_addconductorprop('HighVoltage',471000,0,1);
+  ei_addconductorprop('Ground',0,0,1);
   
-  ei_addboundprop('boundary',V,0,0,0,0);
+  ei_addboundprop('boundary',0,0,0,0,0);
   
   %bottom
   ei_drawrectangle(0,0,140,2000);
@@ -18,6 +19,10 @@ function C = Aufgabe6_2a (V)
   ei_selectlabel(70,1000);
   ei_setblockprop('<No Mesh>',0,0,0);
   ei_clearselected;
+  ei_selectsegment(140,1000);
+  ei_setsegmentprop('None', 0,1,0,0,'Ground');
+  ei_clearselected;
+  
   
   %white block
   ei_drawrectangle(0,2000,115,2120);
@@ -119,19 +124,34 @@ function C = Aufgabe6_2a (V)
   ei_selectlabel(10,7000);
   ei_setblockprop('<No Mesh>',0,0,0);
   ei_clearselected;
-  ei_selectrectangle(0,5600,20,9000);
-  ei_setsegmentprop('None', 0,1,0,0,'HighVoltage');
+  ei_selectsegment(20,7500);
+  ei_selectsegment(90,5600);
+  ei_selectsegment(115,5540);
+  ei_selectsegment(107,5480);
+  ei_setsegmentprop('None',0,1,0,0,'HighVoltage');
   ei_clearselected;
   
+  
 
-  %ring
-  ei_drawarc(565,4800,565,4870,180,1);
-  ei_selectarcsegment(565,4800);
+%   %ring
+%   ei_drawarc(565,4800,565,4870,180,1);
+%   ei_selectarcsegment(565,4800);
+%   ei_setarcsegmentprop(10,'None',0,0,'HighVoltage');
+%   ei_copyrotate2(565,4835,180,1,3);
+%   ei_clearselected;
+%   ei_addblocklabel(565,4835);
+%   ei_selectlabel(565,4835);
+%   ei_setblockprop('<No Mesh>',0,0,0);
+%   ei_clearselected;
+  
+  %ring with user input 
+  ei_drawarc((d/2)-35,z,(d/2)-35,z+70,180,1);
+  ei_selectarcsegment((d/2)-35,z);
   ei_setarcsegmentprop(10,'None',0,0,'HighVoltage');
-  ei_copyrotate2(565,4835,180,1,3);
+  ei_copyrotate2((d/2)-35,z+35,180,1,3);
   ei_clearselected;
-  ei_addblocklabel(565,4835);
-  ei_selectlabel(565,4835);
+  ei_addblocklabel((d/2)-35,z+35);
+  ei_selectlabel((d/2)-35,z+35);
   ei_setblockprop('<No Mesh>',0,0,0);
   ei_clearselected;
   
@@ -141,10 +161,32 @@ function C = Aufgabe6_2a (V)
   ei_selectlabel(2000,6000);
   ei_setblockprop('Vakuum',0,0,0);
   ei_clearselected;
+  ei_selectsegment(2000,0);
+  ei_setsegmentprop('None',0,1,0,0,'Ground');
+  ei_clearselected;
+  ei_selectsegment(0,1000);
+  ei_selectsegment(70,0);
+  ei_selectnode(0,0);
+  ei_selectsegment(0,7500);
+  ei_deleteselected;
+  ei_clearselected;
+  ei_selectsegment(4000,6000);
+  ei_selectsegment(2000,12000);
+  ei_setsegmentprop('boundary',0,1,0,0,'None');
+  ei_clearselected;
   
-  
-  
-  
+  %Segmente für feineres Netz auswählen
+  ei_selectsegment(0,2050);
+  ei_selectsegment(0,2600);
+  ei_selectsegment(0,3140);
+  ei_selectsegment(0,3260);
+  ei_selectsegment(0,3800);
+  ei_selectsegment(0,4350);
+  ei_selectsegment(0,4450);
+  ei_selectsegment(0,5050);
+  ei_selectsegment(0,5550);
+  ei_setsegmentprop('None',meshnet,0,0,0,'None');
+  ei_clearselected;
  
   %Speichere die Datei ab
   ei_saveas('Aufgabe6_2a.FEE');
@@ -153,9 +195,16 @@ function C = Aufgabe6_2a (V)
   ei_analyze(0);
   ei_loadsolution;
   
+  F = zeros(3600,1);
+  E = [0,0];
+  for y = 2000 : 5600 
+    E = eo_gete(0,y);
+    F(y-1999) = -E(2);
+  end
+  
   %Liegenden Vektor erstellen, Ladung / Spannung ergibt Kapazit\"at C
   %G = [0,0];
   %G = eo_getconductorproperties('Rand1');
   %C = G(1,2)/G(1,1);
-  
-endfunction
+  F;
+end
---
+++ Uebung06/data/SkriptAg6_2.m
@@ -0,0 +1,65 @@
+%  A = zeros(3601,60);
+%    B = zeros(3601,20);
+%    R = zeros(3601,1);
+%     counter = 1;
+% %   
+%    for i = 0.1  : -0.001 : 0.08
+%      R = Aufgabe6_2a(1200,4800,i);
+%      for j = 1 : 1 : 3601
+%        B(j,counter) = R(j,1);
+%      end
+%      counter = counter + 1;
+%     end
+% 
+% plot(A(:,1));
+% title('Combine Plots');
+% 
+% hold on
+% 
+% for i = 1 : 64
+%   plot(A(:,i));
+% endfor
+% 
+% %axis([3400 3500]);
+% xlabel("Höhe des Messpunktes in mm","Fontsize",24);
+% ylabel("Stärke des elektrostatischen Felds in V/m","Fontsize",24);
+% hold off
+% 
+% 
+% %R = Aufgabe6_2a(1200,4800,4);
+% 
+% hold on
+% 
+% plot(A(:,60));
+% plot(R);
+% 
+% legend("Feld ohne Randbedingung","Feld mit Randbedingung Potential 0V");
+% h = legend('Feld ohne Randbedingung','Feld mit Randbedingung', "location", "northwest");
+% set(h,'Fontsize',20,'color','none');
+% legend boxoff;
+% xlabel("Höhe des Messpunktes in mm","Fontsize",18);
+% ylabel("Stärke des elektrostatischen Felds in V/m","Fontsize",18);
+% set(gca,"fontsize",18);
+% hold off
+
+% for d = 600 : 25 : 2000
+%   R = Aufgabe6_2a(d,4800,1000);
+%   for j = 1 : 1 : 3601
+%     A(j,counter) = R(j,1);
+%   endfor
+%    counter += 1; 
+% endfor
+
+%A = dlmread('C:\Users\simon\OneDrive\Desktop\Uni\ENBEF\Git\ENBEF Abgaben\Uebung06\data\A60.txt');
+%surf(600:25:2000,2000:5600,A(:,1:57));
+hold on
+plot(C(3400,:))
+hold off
+xlabel("Höhe des Messpunktes","Fontsize",18);
+ylabel("Stärke des elektrischen Felds in V/m","Fontsize",18);
+%l = legend('Elektrisches Feld ohne Randbedinung','Elektrisches Feld mit Randbedingung',"location", "northwest");
+%set(l,'Fontsize',18,'color','none');
+set(gca,'Fontsize',18);
+
+%legend boxoff;
+%xlabel("Außendurchmesser des Rings in mm","Fontsize",18);
