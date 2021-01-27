a956ce743be2f3287eb30b60a8a62e765de4fa6f
Dominik <dominik.schiller@stud.tu-darmstadt.de>
2021-01-27 11:48:30 +0100

    8_3dplots

---
+++ Uebung08/data/SkriptAg8_3d.m
@@ -0,0 +1,12 @@
+%result = zeros(150,1);
+counter = 1;
+%for i = 0:100:15000
+  %  result(counter,1) = SIS(i);
+ %   counter = counter + 1;
+%end  
+plot(result);
+ylabel('Induktivität in H');
+xlabel('Strom in A');
+xtick=get(gca,'xtick');
+xTickLabels = cellstr(num2str(100*xtick(:)));
+set(gca,'fontsize' ,20,'XTick',xtick,'XTickLabel',xTickLabels);
