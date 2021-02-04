3b0fe7d59e77f77184e995decb387784b3879a53
Simon <simonarnold@live.de>
2021-02-04 08:40:22 +0100

    ------------------Beginn Abgabe 10---------------------

---
+++ Uebung10/data/trafo_linear.m
@@ -0,0 +1,78 @@
+% get data
+load trafo_linear.mat;
+
+% get mesh data
+nx = length(prb.xmesh);
+ny = length(prb.ymesh); 
+nz = length(prb.zmesh);
+
+% get and shrink conductivity matrix
+Msigma = prb.Msigma;
+Msigma = Msigma(prb.idxdof,prb.idxdof);
+M = Msigma;
+
+% ---- do something here ---- 
+% construct curl-matrix
+C = sparse(3*nx*ny*nz,3*nx*ny*nz);
+% ---- do something here ---- 
+
+% create and shrink curl-curl matrix
+K = C'*diag(prb.Nu)*C;
+K = K(prb.idxdof,prb.idxdof);
+
+% get and shrink excitation matrix
+X = prb.Qstr;
+X = X(prb.idxdof,:);
+
+% time parameters
+T0    = 0.00;
+Tend  = 0.02;
+dt    = 1e-3; % try dt=1e-4 if your computer is fast.... 
+
+% discrete time vector
+T    = T0:dt:Tend;
+
+% initialize solution
+f     = 50;
+a     = zeros(size(M,1),length(T));
+i     = @(t) (10*[sin(2*pi*f*t);0*t]);
+
+% construct preconditioner for pcg
+% use "pcg(A,b,1e-6,1000,Z,Z)" to solve 
+% the linear problem Ax=b for x
+Z = sparse(1:size(a,1),1:size(a,1),sqrt(diag(M/dt+K)));
+
+% time loop with equidistant time steps
+for jj=2:length(T)
+
+  % ---- do something here ---- 
+  % solve M*a'+K*a=X*i with the implicit Euler method 
+  a(:,jj)=a(:,jj);  
+  % ---- do something here ---- 
+
+end
+
+
+% Postprocessing 1: plot the current excitation i(t)
+% ---- do something here ---- 
+plot(0,0);
+ % ---- do something here ---- 
+
+% Postprocessing 2: return the fluxes B!
+A = zeros(length(prb.ds),1);
+B = zeros(length(prb.ds),1);
+for jj=1:length(T)
+  A(prb.idxdof) = a(:,jj);
+  
+  % ---- do something here ---- 
+  % compute the facet integrated fluxes b
+  B = 0*A; 
+  % ---- do something here ---- 
+    
+  % scaling and write to file
+  A(prb.idxs) = A(prb.idxs)./prb.ds(prb.idxs); 
+  B(prb.idxA) = B(prb.idxA)./prb.dA(prb.idxA); 
+  fit_write_vtk (prb.xmesh,prb.ymesh,prb.zmesh,['solution_' num2str(jj,'%03d') '.vtr'],...
+                 {'MVP',A;'Flux',B},{'Region',prb.Elem(prb.idxV);});
+end
+% visualize the result in Paraview, e.g. onprb a cutplane
---
+++ Uebung10/data/trafo_nichtlinear.m
@@ -0,0 +1,105 @@
+% lese Eingabedaten
+load trafo_nichtlinear.mat;
+
+% Gittertopologie
+xmesh = prb.xmesh;
+ymesh = prb.ymesh;
+zmesh = prb.zmesh;
+nx = length(xmesh);
+ny = length(ymesh); 
+nz = length(zmesh);
+Np = nx*ny*nz;
+material = prb.Elem;
+
+% Gitterabmessungen und Indizes
+ddA     = prb.ddA;           % duale Flaecheninhalte
+dA      = prb.dA;            % primale Flaecheninhalte
+ds      = prb.ds;            % primale Kantenlaengen
+idxV    = prb.idxV;          % Indizes der primalen Volumina im Rechengebiet 
+idxA    = prb.idxA;          % Indizes der primalen Flaechen im Rechengebiet 
+idxs    = prb.idxs;          % Indizes der primalen Kanten im Rechengebiet 
+idxdof  = prb.idxdof;        % Indizes der Freiheitsgrade 
+idxiron = find(material==6); % Indizes fuer Eisen
+
+% Operatoren
+C = prb.C;       % Curl-Matrix
+
+% Anregungsvektor mit 10A Strom pro Spule
+jsrc  = prb.Qstr*[1;1];
+jdof  = jsrc(idxdof);
+ 
+% Reluktivitaet 
+mu0 = 4*pi*10^-7;
+nu  = ones(Np,1)/mu0;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Linear
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Loese lineares Problem
+nu(idxiron) = ...; 
+Mnu         = createMnu(...);
+K           = ...;
+Kdof        = K(...);
+Zdof        = sparse(1:size(Kdof,1),1:size(Kdof,2),sqrt(diag(Kdof)));
+a           = zeros(3*Np,1);
+a(idxdof)   = pcg(Kdof,jdof,1e-7,1000,Zdof,Zdof);
+
+% berechne magnetische Flusse im Volumen
+b           = ...;
+B           = zeros(3*Np,1);
+B(idxA)     = b(idxA)./dA(idxA);
+Bc          = fit_pf2pc(xmesh,ymesh,zmesh,B,idxV);
+
+% Ausgabe
+fit_write_vtk(xmesh,ymesh,zmesh,['trafo_iter' num2str(0) '.vtr'],{},{'Flussdichte',Bc(idxV,:);'Material',material(idxV)})
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Nichtlinear 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Parameter fuer Brauer's Kurve
+k1  = 0.3774;
+k2  = 2.970; 
+k3  = 388.33;
+
+% Startwerte fuer die Fixpunktiteration 
+aold = zeros(3*Np,1);
+a    = zeros(3*Np,1);
+i    = 0;
+
+% Fixpunkt iteration
+while (i<2) | ...
+	
+	% update der Variablen
+	aold=...;
+	i=...;
+	fprintf('Iteration %d\n',i);
+	
+	% berechne magnetische Flusse im Volumen
+	b       = ...;
+	B       = zeros(3*Np,1);
+	B(idxA) = b(idxA)./dA(idxA);
+	Bc      = fit_pf2pc(xmesh,ymesh,zmesh,B,idxV);
+	
+	% Auswertung der Materialkurve
+	Biron   = sqrt(sum( Bc(idxiron,:).^2, 2) );
+	nu(idxiron) = ...;
+	
+	% Update der Materialmatrix
+	Mnu = createMnu(...);
+
+	% create and shrink curl-curl matrix
+	K = ...;
+	Kdof = K(...);
+
+	% loese GLS
+	Zdof = sparse(1:size(Kdof,1),1:size(Kdof,2),sqrt(diag(Kdof)));
+	a(idxdof) = pcg(Kdof,jdof,1e-7,1000,Zdof,Zdof,a(idxdof));
+
+	% Ausgabe
+	fit_write_vtk(xmesh,ymesh,zmesh,['trafo_iter' num2str(i) '.vtr'],{},{'Flussdichte',Bc(idxV,:);'Material',material(idxV)})
+
+end
+
+
